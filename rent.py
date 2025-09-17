4feb4d80a710632e2754f2f0550f09279ba0acaa
Ramrup Satpati <ramrupsatpati@gmail.com>
2025-09-17 18:12:52 +0530

    Added a new Rent calculator Project into the Repository - Idea given by Sagar Chouksey

---
+++ rent.py
@@ -0,0 +1,10 @@
+rent = abs(int(input("Enter the hostel/flat rent here : ")))
+food = abs(int(input("Enter the fooding expenses here : ")))
+electricity_spent = abs(int(input("Enter the electricity units spent by the hostel/flat room here : ")))
+units = abs(int(input("Enter the cost per unit according to the Standand Indian Bill Rate : ")))
+persons = abs(int(input("Enter the number of residents living in the hostel : ")))
+bill = electricity_spent * units
+output = (rent + food + bill) / persons
+output_app = (rent + food + bill) // persons 
+print(f"The per resident rent is : {output}")
+print(f"But in order to resolve conflict the approximate rent is : {output_app}")
