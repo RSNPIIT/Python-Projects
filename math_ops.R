fa2551733fede7fef4cc4b95ee30d979678d0745
Ramrup Satpati <ramrupsatpati@gmail.com>
2025-09-17 15:33:37 +0530

    This is me Commiting to include a file math_ops.R in order to add an easter egg about R as R is a suoer underrated language

---
+++ math_ops.R
@@ -0,0 +1,21 @@
+# math_ops.R
+args <- commandArgs(trailingOnly = TRUE)
+
+if(length(args) < 2){
+  stop("Please provide two numbers as arguments, e.g. Rscript math_ops.R 12 5")
+}
+
+num1 <- as.numeric(args[1])
+num2 <- as.numeric(args[2])
+
+sum_result <- num1 + num2
+prod_result <- num1 * num2
+diff_result <- num1 - num2
+div_result <- num1 / num2
+mod_result <- num1 %% num2
+
+cat("Sum: ", sum_result, "\n")
+cat("Product: ", prod_result, "\n")
+cat("Difference: ", diff_result, "\n")
+cat("Division: ", div_result, "\n")
+cat("Remainder: ", mod_result, "\n")
