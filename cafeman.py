ca28f6cea9038d1936f0f98cd0e174254b5764da
Ramrup Satpati <ramrupsatpati@gmail.com>
2025-09-23 00:54:43 +0530

    I have changed the code of The Cafe/Restaurant manager app to be more intuitive

--- cafeman.py
+++ cafeman.py
@@ -1,4 +1,4 @@
-#This mini peoject I have built to revise Dictionaries and conditional statements
+#This mini peoject I have built to revise Dictionaries and conditional statements and Looping Iterators
 menu = {
     'pizza' : 40,
     'burger' : 50,
@@ -14,35 +14,37 @@ menu = {
     'fish' : 64,
     'egg' : 46
 }
-print("Welcome to Python Restaurant , Pytho is Elated to Welcome you")
-print("This Restaurant belongs to the same chain ass the FOSS , same as Tux Rest. and Bugdroid's Rest.")
-print("The Menu is :\nPizza : 40\nBurger : 50\nSalad : 20\nFries : 15\nPasta : 60\nNoodles : 55\nCoffee : 35\nBiriyani : 90\nFried Rice : 65\nToast : 20\nNuggets : 45\nFish : 64\nEgg : 46")
-print("What would you like to Order ? , आप क्या मंगवाना पसंद करेंगे ? :")
-order = input("Enter your Order Here :")
 price = 0
-key = order.strip().lower()
-if key in menu:
-    price += menu[key]
-    print("Do You want something else also ?")
-    choice = input("Enter your Choice Yes/No :")
-    xe = choice.strip().lower()
-    if xe == "yes":
-        sec_ord = input("And please make your second order :")
-        ya = sec_ord.strip().lower()
-        if ya in menu:
-            price += menu[ya]
-            print("Your Total Cost to Pay is : ",price)
-        else:
-            print(f"Sorry we dont have {sec_ord} yet. Please check again later")
-            print(f"The Net Price is : {price}")
-    elif (xe == "no") or (xe == "nope"):
-        print("Alight No Problem ")
-        print("The Total is :",total)
+print("The Menu is :\nPizza : 40\nBurger : 50\nSalad : 20\nFries : 15\nPasta : 60\nNoodles : 55\nCoffee : 35\nBiriyani : 90\nFried Rice : 65\nToast : 20\nNuggets : 45\nFish : 64\nEgg : 46")
+print("This Restaurant belongs to the same chain ass the FOSS , same as Tux Rest. and Bugdroid's Rest.")
+flag = False
+while not flag:
+    order = input("Enter your Order Here :").strip().lower()
+    if order in menu:
+        price += menu[order]
+        print("The Menu is :\nPizza : 40\nBurger : 50\nSalad : 20\nFries : 15\nPasta : 60\nNoodles : 55\nCoffee : 35\nBiriyani : 90\nFried Rice : 65\nToast : 20\nNuggets : 45\nFish : 64\nEgg : 46")
+        print(f"The order {order} is placed successfully")
+        while True:
+            print("Now You make a choice | The Choices are :\n1 - Make another Order\n2 - Exit\n")
+            choice = abs(int(input("Enter a Choice Here : ")))
+            if choice == 1:
+                sec_ch = input("Enter the second choice of menu : ").strip().lower()
+                if sec_ch in menu:
+                    price += menu[sec_ch]
+                    print(f"Congratulations {sec_ch} has been placed\n")
+                else:
+                    print("The selected item is unavailable currently , Please check again later\n")
+        
+            elif choice == 2:
+                if price > 500:
+                    price = 0.9 * price
+                print(f"--Your Grand Total is : {price} ---")
+                print("-- Thankyou Very Much For Visiting Our Restaurant --")
+                flag = True
+                break
+            else:
+                print("-- Wrong Choice --")
+                continue 
     else:
-        print("ERR : Invalid Choice")
-else:
-    print(f"Sorry we dont have {order} yet. Please check again later")
-print("Thankyou for Shopping with us")
-        # This file is part of Python-Projects.
-# Copyright (c) 2025 Ramrup Satpati
-# Licensed under the GNU General Public License v3.0 (GPL-3.0).
+        print("Your Order is not in Menu , Maybe try something else")
+        continue        
