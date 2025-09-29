8d1ba9250666a4eca750b62611c93cfe0f52a4f7
Ramrup Satpati <ramrupsatpati@gmail.com>
2025-09-29 23:11:37 +0530

    My new Project based off of the hangman iteration , albeit with a polished CLI UI

---
+++ hangman.py
@@ -0,0 +1,117 @@
+import random
+print(r"""
+ _   _                                         
+| | | |                                        
+| |_| | __ _ _ __   __ _ _ __ ___   __ _ _ __  
+|  _  |/ _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+\_| |_/\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/                       
+""")
+word_list = ["Elephant", "Capsuley", "Overcome", "Driftwood",
+         "Bravados", "Landlord", "Overkill", "Junction",
+         "Pastoral", "Velocity"]
+stages = [
+    """
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+"""
+]
+word = random.choice(word_list).strip().lower()
+correct_l = []
+#print(word)
+d = ""
+for _ in range(len(word)):
+    d += '_'
+print(f"*********Your Word is : {d}***************\n**********HINT : IT IS AN 8 LETTERED WORD*********")
+lives = 6
+game_over = False
+while not game_over:
+    print(f"*************{lives}/6 LIVES***********")
+    guess = input("Enter Your Guess Letter Here : ").strip().lower()
+    display = ""
+
+    for letter in word:
+        if guess == letter:
+            display += letter
+            correct_l.append(letter)
+        elif letter in correct_l:
+            display += letter    
+        else:
+            display += '_'
+    print(display)
+    
+    if guess not in word:
+        lives -= 1
+        print(f"Aah , You Lose One Life , You have {lives} left")
+        if lives == 0:
+            game_over = True
+            print(f"***************** IT WAS {word} ALL THIS TIME / YOU LOSE *****************")
+            print("Sorry You Lose , You Ran Out of Lives")
+    if '_' not in display:
+        game_over = True
+        print("You Win")
+    if guess in correct_l:
+        lives -= 0
+        print("You have Already Guessed the Word , Guess Anything Else")
+    print(stages[6-lives])
