7f8c7d3b8c17c259119ad2a2cc21ac73a764e2d7
Ramrup Satpati <ramrupsatpati@gmail.com>
2025-10-01 16:22:40 +0530

    Another project added to the evolving list of more and more projects

---
+++ ceasercipher.py
@@ -0,0 +1,53 @@
+print('''
+ ▗▄▄▖▗▄▄▄▖ ▗▄▖  ▗▄▄▖▗▄▄▄▖▗▄▄▖          ▗▄▄▖▗▄▄▄▖▗▄▄▖ ▗▖ ▗▖▗▄▄▄▖▗▄▄▖ 
+▐▌   ▐▌   ▐▌ ▐▌▐▌   ▐▌   ▐▌ ▐▌        ▐▌     █  ▐▌ ▐▌▐▌ ▐▌▐▌   ▐▌ ▐▌
+▐▌   ▐▛▀▀▘▐▛▀▜▌ ▝▀▚▖▐▛▀▀▘▐▛▀▚▖        ▐▌     █  ▐▛▀▘ ▐▛▀▜▌▐▛▀▀▘▐▛▀▚▖
+▝▚▄▄▖▐▙▄▄▖▐▌ ▐▌▗▄▄▞▘▐▙▄▄▖▐▌ ▐▌        ▝▚▄▄▖▗▄█▄▖▐▌   ▐▌ ▐▌▐▙▄▄▖▐▌ ▐▌
+                                                            
+''')
+print("UDEMY DAY 8 Python Project Given by @Angela Yu")
+print("Welcome to Ramrup's Ceaser Cypher Project ")
+def encoder(word,num):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    xq = ""
+    for char in word:
+        if char not in alphabet:  
+            xq += char
+        else:
+            x = alphabet.index(char)
+            sum_val = (x + num) 
+            sum_val %= len(alphabet)
+            xq += alphabet[sum_val]
+    print(f"The Encoded Value is: {xq}")
+def decoder(word,num):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    xq = ""
+    for char in word:
+        if char not in alphabet:  
+            xq += char
+        else:
+            x = alphabet.index(char)
+            sum_val = (x - num) 
+            sum_val %= len(alphabet)
+            xq += alphabet[sum_val]
+    print(f"The Decoded Value is: {xq}")
+attmpts = 0
+while True:
+    print("\n1.For Encoding\n2.For Decoding\n3.To Exit")
+    choice = abs(int(input("Enter Your Choice Here : ")))
+    if choice == 1:
+        word = input("Enter Your Message Here : ").lower()
+        attmpts += 1
+        num = abs(int(input("Enter the Value By What You wanna Shift To Encode the Message : ")))
+        encoder(word,num)
+    elif choice == 2:
+        word = input("Enter Your Message Here : ").lower()
+        attmpts += 1
+        num = abs(int(input("Enter the Value By which You Wanna Shift (or Rarther DeShift) to Decode the message : ")))
+        decoder(word,num)
+    elif choice == 3:
+        print(f"It was fun having you , You've used this app for {attmpts} times")
+        break
+    else:
+        print("Wrong Choice , Please Choose Something Else")
+        continue
