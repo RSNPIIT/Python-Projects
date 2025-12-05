6df30dc9e9867e53ee18caa5a62fb130035afbc9
Ramrup Satpati <ramrupsatpati@gmail.com>
2025-12-05 11:51:03 +0530

    Modified the Project to have a better UI on CLI

--- NATO_CODE-HUNT/main.py
+++ NATO_CODE-HUNT/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 data = pd.read_csv("nato_phonetic_alphabet.csv")
+print('The NATO Code For your sake is : \n')
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
@@ -9,10 +10,11 @@ def generate_phonetic():
     try:
         output_list = [phonetic_dict[l] for l in word]
     except KeyError :
-        print("Only Letters Are Allowed")
+        print("Only Letters Are Allowed\n")
         generate_phonetic()
     else:
-        print(output_list)
+        print("The NATO Alphabet Encryption of your Word is :-\n")
+        for i in output_list:
+            print(i , end = ' ')
 
 generate_phonetic()
-
