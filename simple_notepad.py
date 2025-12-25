b5d3f339715e0ee66c98b173cef8e5b09811a7df
RSNPIIT <ramrupsatpati@gmail.com>
2025-12-25 21:16:21 +0530

    Updated some logic flaw

--- simple_notepad.py
+++ simple_notepad.py
@@ -1,13 +1,13 @@
 import tkinter as tk
 from tkinter import ttk
 
-#TTK Widgets
-lbl = ttk.Label(master = win , text = 'NOTEPAD').pack()
-
 #Widgets
 win = tk.Tk() #A Window to Place Everything in
 win.title('Window and Widgets')
 win.geometry('700x700') #This has values in height x width format
+
+#TTK Widgets
+lbl = ttk.Label(master = win , text = 'NOTEPAD').pack()
 
 #Text Widgets
 tex = tk.Text(master = win)
