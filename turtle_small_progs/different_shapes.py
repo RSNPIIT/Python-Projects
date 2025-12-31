6c291ed9e0ed8bd56ab90c5b7d65082bd5d674d4
RSNPIIT <ramrupsatpati@gmail.com>
2025-12-31 12:53:01 +0530

    Fixed some improvements in the UI

--- turtle_small_progs/different_shapes.py
+++ turtle_small_progs/different_shapes.py
@@ -1,17 +1,30 @@
 import turtle as t
 
-#Setup the Screen
-sc = t.Screen()
-sc.setup( 800 , 800)
-sc.bgcolor('black')
-sc.title('Shapes Revision')
+#Asking as to how big each shape's gonna be
+x = abs(int(input("Enter the Size of each shape : ")))
 
-#Setup the moving turtle
-timmy = t.Turtle()
-timmy.color('white')
-timmy.hideturtle()
-timmy.home()
+if x <= 100 and x >= 20:
+    #Setup the Screen
+    sc = t.Screen()
+    sc.setup( 800 , 800)
+    sc.bgcolor('black')
+    sc.title('Shapes Revision')
 
+    #Setup the moving turtle
+    timmy = t.Turtle()
+    timmy.color('white')
+    timmy.hideturtle()
+    timmy.home()
+
+    #All Shapes 
+    for size in range(3 , 11):
+        draw_shape(size)
+
+elif x < 20:
+    print("\nToo Small \nCan't Make - This will be invisibe")
+else:
+    print("\nToo Big \nCan't Make this this will go out of bounds")
+    
 #Make the Shapes
 def draw_shape(siz):
     ang = 360 // siz
@@ -19,8 +32,4 @@ def draw_shape(siz):
         timmy.forward(100)
         timmy.right(ang)
         
-#All Shapes 
-for size in range(3 , 11):
-    draw_shape(size)
-
 sc.exitonclick()
