9a9ac41f2145c88d3768e5f99b918211e84b1a05
RSNPIIT <ramrupsatpati@gmail.com>
2026-02-23 19:46:00 +0530

    Updated the Boilerplate code

--- selenium_intro.py
+++ selenium_intro.py
@@ -14,15 +14,15 @@ from selenium import webdriver as wbd
 # - Edge (Chromium-based)
 # It does NOT work for Firefox (use webdriver.Firefox()).
 
-driver = wbd.Chrome()
+#BoilerPlate to prevent the Chrome(-ium) like Browsers from closing after the program finishes
+chr_opt = wbd.ChromeOptions()
+chr_opt.add_experimental_option("detach" , True)
+
+driver = wbd.Chrome(options = chr_opt)
 driver.get("https://www.amazon.com")
 ti.sleep(3)
-# Initialize WebDriver for Chromium-based browsers.
-# This works for:
-# - Chromium
-# - Google Chrome
-# - Brave
-# - Edge (Chromium-based)
-# It does NOT work for Firefox (use webdriver.Firefox()).
 
-driver.quit()
+#Now This is about closing the browser since we are trying to prevent quitting so these are commented out
+
+# driver.close()  -> closes only the active tab
+# driver.quit()   -> terminates entire browser session and driver process
