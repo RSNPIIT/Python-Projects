e3adbdbed3bc1858b5e5f3287abf2d84f710289e
RSNPIIT <ramrupsatpati@gmail.com>
2026-03-10 17:29:59 +0530

    Added a Class Revision Prog. Project here

---
+++ area_class.py
@@ -0,0 +1,22 @@
+class Circle():
+    def __init__(self , rad):
+        self.pi = 3.141
+        self.rad = rad
+    
+    def area(self):
+        return self.pi * (self.rad ** 2)
+    
+    def per(self):
+        return 2*self.pi*self.rad
+
+try:
+    ra = abs(float(input("Enter the Radius Here :")))
+except ValueError as v:
+    print("\nNon Integral Values Given\n")
+    exit()
+else:
+    myX = Circle(ra)
+    print(f"The Area is : {myX.area()} ")
+    print(f"The Area is : {myX.per()} ")
+finally:
+    print("This is the Circle")
