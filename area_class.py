49d4489b602c50ccff10f98eb814296db7e9e35a
RSNPIIT <ramrupsatpati@gmail.com>
2026-03-13 08:43:14 +0530

    Updated the Code to have a great UI

--- area_class.py
+++ area_class.py
@@ -1,3 +1,4 @@
+import turtle as t
 import time as ti
 import os as o
 
@@ -22,10 +23,31 @@ except (KeyboardInterrupt, EOFError):
     exit()
 except ValueError as v:
     print("\nNon Integral Values Given\n")
+    ti.sleep(1)
+    o.system('cls' if o.name == 'nt' else 'clear')
     exit()
 else:
     myX = Circle(ra)
     print(f"The Area is : {myX.area()} ")
     print(f"The Circumference is : {myX.per()} ")
+
+
+    print("\nVisualizing the Circle ->\nModule Loading....")
+    ti.sleep(1)
+    print("\nModule Loaded....\nProceeding....")
+    ti.sleep(1)
+    print("\nDone")
+
+    sc = t.Screen()
+    sc.setup(800 , 800)
+    sc.title('Circle Visualizing')
+    sc.bgcolor('black')
+
+    timmy = t.Turtle()
+    timmy.color("yellow")
+    timmy.pensize(3)
+    timmy.circle(ra)
+
+    sc.exitonclick()
 finally:
     print("This is the Circle")
