4ee4cd4209b554d7f7e5cac95277af4f22e29873
RSNPIIT <ramrupsatpati@gmail.com>
2026-03-16 19:45:02 +0530

    The Code has been Revamped to have a better UI

--- chudnovsky_math_calc/accurate_picalc.py
+++ chudnovsky_math_calc/accurate_picalc.py
@@ -2,6 +2,15 @@ import decimal as dc
 import math as m
 import time as ti
 import os as o
+import sys as s
+import turtle as t
+import datetime as dt
+
+#Static Variables
+now = dt.datetime.now()
+yr = now.year
+ALIGNV = 'center'
+FONT = ('Arial' , 25 , 'bold')
 
 #The Function to get an accurate Pi upto a large number of digits
 def the_accurate_pizza(digi):
@@ -9,7 +18,7 @@ def the_accurate_pizza(digi):
         dc.getcontext().prec = digi + 2
     except OverflowError as l:
         print(f"\nToo Large Value Given\n{l}\n")
-        exit()
+        s.exit()
 
     #Chudnovsky Constant for Incrementing Values
     chud_cons = (digi // 14) + 1
@@ -35,27 +44,52 @@ try:
         print("Cant Show Pi Upto 0 Decimal Places\nPlease Re-Enter\n")
         ti.sleep(1)
         o.system('cls' if o.name == 'nt' else 'clear')
-        exit()
+        s.exit()
 
     elif dig > 1500:
         print("\nFor Safety reasons we have capped all of it at 1500 digits so as to avoid CPU Lags and stutters\n")
         ti.sleep(1)
         o.system('cls' if o.name == 'nt' else 'clear')
-        exit()
+        s.exit()
     
 except (KeyboardInterrupt , EOFError) as kb:
     print("\nExitting the Code\nPlease Dont Spam....\n")
     ti.sleep(1)
     o.system('cls' if o.name == 'nt' else 'clear')
-    exit()
+    s.exit()
 except ValueError as v:
     print("\nNon Integral Values are not allowed\nPlease Re-Enter\n")
     ti.sleep(1)
     o.system('cls' if o.name == 'nt' else 'clear')
-    exit()
+    s.exit()
 else:
     giv_val = the_accurate_pizza(dig)
     print(f"For {dig} decimal place(s) the Pi Value is :- \n{giv_val}\n")
+
+    ti.sleep(1)
+    print("Making the Circle...")
+
+    # Screen Setup
+    sc = t.Screen()
+    sc.setup(800,800)
+    sc.title('The Accurate Pizza')
+    sc.bgcolor('black')
+
+    # Turtle Object Setup
+    timmy = t.Turtle()
+    timmy.shape('turtle')
+    timmy.color('yellow')
+    timmy.pensize(3)
+    timmy.hideturtle()
+    timmy.circle(150)
+    ti.sleep(1)
+    timmy.home()
+    timmy.clear()
+    timmy.write(f'The [PI]zza is Served\nHappy Pi Day {yr}' , align = ALIGNV , font = FONT)
+
+    sc.exitonclick()
+    print(f"The Circle is Made\nHappy PI Day {yr}")
+
 finally:
     print("\n🄯 Ramrup Satpati (RSNPIIT) | IIT Madras")
     print("Licensed under GNU GPL-3 (Copyleft)")
