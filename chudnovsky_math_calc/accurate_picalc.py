e393df86ac33203bdea105873a8375157be8dc7a
RSNPIIT <ramrupsatpati@gmail.com>
2026-03-16 19:12:37 +0530

    Added the Chudnovsky Math Calculator Code

---
+++ chudnovsky_math_calc/accurate_picalc.py
@@ -0,0 +1,62 @@
+import decimal as dc
+import math as m
+import time as ti
+import os as o
+
+#The Function to get an accurate Pi upto a large number of digits
+def the_accurate_pizza(digi):
+    try:
+        dc.getcontext().prec = digi + 2
+    except OverflowError as l:
+        print(f"\nToo Large Value Given\n{l}\n")
+        exit()
+
+    #Chudnovsky Constant for Incrementing Values
+    chud_cons = (digi // 14) + 1
+    pi_v = dc.Decimal(0)
+
+    for k in range(chud_cons):
+        numerator = ((-1) **k) * m.factorial(6 *k) * (13591409 + 545140134 *k)
+
+        denominator = m.factorial(3 *k) * (m.factorial(k) ** 3) * 640320 ** (3 * k)
+
+        pi_v += dc.Decimal(numerator) / dc.Decimal(denominator)
+
+    const_for_cal = dc.Decimal(426880) * (dc.Decimal(10005)) ** (dc.Decimal(0.5))
+    pi_val = const_for_cal / pi_v
+    return +pi_val
+
+#User Input
+print("\nWelcome to the Python and Linux Pi Accurate Digit Simulator\nTo Get Started Please Enter the Number of Digits to see pi to\nRECOMMENDED :- (15 digits to 1500 digits)\n")
+try:
+    dig = abs(int(input("Enter the Number of Digits You wanna calculate Pi to :-> ")))
+
+    if dig == 0:
+        print("Cant Show Pi Upto 0 Decimal Places\nPlease Re-Enter\n")
+        ti.sleep(1)
+        o.system('cls' if o.name == 'nt' else 'clear')
+        exit()
+
+    elif dig > 1500:
+        print("\nFor Safety reasons we have capped all of it at 1500 digits so as to avoid CPU Lags and stutters\n")
+        ti.sleep(1)
+        o.system('cls' if o.name == 'nt' else 'clear')
+        exit()
+    
+except (KeyboardInterrupt , EOFError) as kb:
+    print("\nExitting the Code\nPlease Dont Spam....\n")
+    ti.sleep(1)
+    o.system('cls' if o.name == 'nt' else 'clear')
+    exit()
+except ValueError as v:
+    print("\nNon Integral Values are not allowed\nPlease Re-Enter\n")
+    ti.sleep(1)
+    o.system('cls' if o.name == 'nt' else 'clear')
+    exit()
+else:
+    giv_val = the_accurate_pizza(dig)
+    print(f"For {dig} decimal place(s) the Pi Value is :- \n{giv_val}\n")
+finally:
+    print("\n🄯 Ramrup Satpati (RSNPIIT) | IIT Madras")
+    print("Licensed under GNU GPL-3 (Copyleft)")
+    print("Thankyou....\n")
