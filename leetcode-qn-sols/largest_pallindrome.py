b9941e39283cba82bc31dde72f1ed8a7f92c6045
RSNPIIT <ramrupsatpati@gmail.com>
2026-03-19 11:12:15 +0530

    Added the Largest Pallindrome Sollution (Accepted by Leetcode)

---
+++ leetcode-qn-sols/largest_pallindrome.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        #Sollution for Empty String
+        if not s:
+            return ""
+
+        long_er = ''
+        for i in range(len(s)):
+            #Sollution for Odd Palindrome
+            l, r = i , i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > len(long_er):
+                    long_er = s[l : r + 1]
+                l -= 1
+                r += 1
+            #Sollution for Even Pallindrome 
+            l, r = i , i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if(r - l + 1) > len(long_er):
+                    long_er = s[l : r + 1]
+                l -= 1
+                r += 1
+        return long_er
