99bd38107a1af15ff142fafc9708345a5e018179
RSNPIIT <ramrupsatpati@gmail.com>
2026-03-23 12:27:41 +0530

    Updated this code as it has some errors and also added guardrails

--- leetcode-qn-sols/pallindrome_numb.py
+++ leetcode-qn-sols/pallindrome_numb.py
@@ -1,3 +1,7 @@
+import time as ti
+import sys as s
+import os as o
+
 class Solution:
     def __init__(self , x):
         self.x = x
@@ -19,27 +23,55 @@ class Solution:
             else:
                 return False
 
-try:
-    num = int(input("Enter the Number you wish to test here : "))
+print("The Pallindrome Goes\nCopyleft :- Ramrup Satpati (GPL-3)\nCredits :- Leetcode\nSollution is released as an idea and is not for cheating purposes")
+attmpt = 0
 
-    if not num:
-        print("You Cant Leave the Feild Blank Like That..\n")
+while True:
+    attmpt += 1
+    try:
+        num = int(input("Enter the Number you wish to test here : "))
+
+        if not num:
+            print("You Cant Leave the Feild Blank Like That..\n")
+            ti.sleep(1)
+            o.system('cls' if o.name == 'nt' else 'clear')
+            s.exit()
+    except (KeyboardInterrupt , EOFError) as kb:
+        print("\nExitting--- Please dont spam..")
         ti.sleep(1)
         o.system('cls' if o.name == 'nt' else 'clear')
         s.exit()
-except (KeyboardInterrupt , EOFError) as kb:
-    print("Exitting--- Please dont spam..")
-    ti.sleep(1)
-    o.system('cls' if o.name == 'nt' else 'clear')
-    s.exit()
-except ValueError as v:
-    print("The Non Integral Values are not allowed here----\nPlease (RE)enter--")
-    ti.sleep(1)
-    o.system('cls' if o.name == 'nt' else 'clear')
-    s.exit()
-else:
-    s1 = Solution(num)
-    val = s1.isPalindrome()
-    print(f"Status :->\nIt is {val} that {num} is Pallindrome\n")
-finally:
-    print("The Pallindrome Goes\nCopyleft :- Ramrup Satpati (GPL-3)\nCredits :- Leetcode\nSollution is released as an idea and is not for cheating purposes")
+    except ValueError as v:
+        print("The Non Integral Values are not allowed here----\nPlease (RE)enter--")
+        ti.sleep(1)
+        o.system('cls' if o.name == 'nt' else 'clear')
+        s.exit()
+    else:
+        s1 = Solution(num)
+        val = s1.isPalindrome()
+        print(f"Status :->\nIt is {val} that {num} is Pallindrome\n")
+
+        print("Wanna Check a New Number (Y/N)")
+        
+        try:
+            ch = input("Enter Your Choice (y/n) :").strip().lower()
+
+            if ch == 'n':
+                print(f"Thankyou for COming and testing the Software\nApp Used for {attmpt} time(s)\nPlease do come back")
+                break   
+            elif ch == 'y':
+                print(f"Good\Lets Test Something else\n")
+                continue
+            else:
+                print(f"Wrong Input---Redirecting to a y\n")
+                ti.sleep(1)
+                print("Redirection Successful\n")
+                continue
+        except (KeyboardInterrupt , EOFError) as kb:
+            print("See comrade -- If you need to exit follow the rules and press y\nExit Blocked via the Keyboard Interrupt pathway\n")
+            continue
+        except Exception as e:
+            print(f"Some Error Occurred which is caught successfully\n{e}")
+            continue
+    finally:
+        print("Pallindrome Goes ...")
