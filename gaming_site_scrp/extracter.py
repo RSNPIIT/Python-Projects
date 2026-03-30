4281f8b7f08320a19f430228d1aa49eae0be14ca
RSNPIIT <ramrupsatpati@gmail.com>
2026-03-31 02:09:38 +0530

    Improved the UI Significantly

--- gaming_site_scrp/extracter.py
+++ gaming_site_scrp/extracter.py
@@ -1,11 +1,12 @@
-from bs4 import BeautifulSoup as bls
 import time as ti
+from bs4 import BeautifulSoup as bls
 
 # Static Variables 
 FILE = "scrper.html"
 PARSER = 'html.parser'
 TFILE = 'saver.txt'
-APMODE = 'a'
+WMOD = 'w'
+ENCD = 'utf-8'
 
 # Extracting the File Here
 with open(FILE) as rf:
@@ -14,15 +15,25 @@ with open(FILE) as rf:
 # Giving the Output to Beautiful Soup
 soup = bls(hgf ,PARSER)
 tilte = soup.title.string
-print(f'The Title is :- \n{tilte.strip()}')
 
 # Now Getting the Titles of the games here
 titl = soup.find_all('h2')
+tup = []
 for t in titl:
     sth = t.find('strong')
     if sth:
-        with open(TFILE , APMODE) as a:
-            a.write(f"{sth.getText().strip()}\n")
+        tex = sth.getText().strip()
+        tup.append(tex)
+
+trt = list(reversed(tup))
+
+with open(TFILE , WMOD , encoding = ENCD) as w:
+    w.write(f"-"*50)
+    w.write(f"\n{tilte}\n")
+    w.write('-'*50)
+    for j in trt:
+        w.write(f'\n{j}\n')
+        w.write('-'*50)
 
 ti.sleep(1)
 print("Successfully Written to the Text File")
