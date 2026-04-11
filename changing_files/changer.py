2526b8583ebf2d7375163dbd2d19305bd6d453cc
RSNPIIT <ramrupsatpati@gmail.com>
2026-04-11 20:24:00 +0530

    Updated the Code to be more user friendly

--- changing_files/changer.py
+++ changing_files/changer.py
@@ -1,41 +1,70 @@
 import os as o
 import time as ti
+import platform as pt
+import sys as s
 
-# Enter a name and extension
-fname = input("Enter the Name of the File Here (For all files) : ").strip()
-fext = input("Enter the Name of Extension of the File Here : ").strip().lower()
+# Static Variables
+LIS = [f for f in o.listdir() if o.path.isfile(f) and not f.endswith(('.py', '.ipynb'))]
+SYM = '-'*50
+itn = 1
 
-if not fext.startswith('.'):
-    print("The File Doesnt Start with a Dot Separator so we add one")
-    fext = '.' + fext
+if not LIS:
+    print("Python Found that the directory is empty with no non pythonic files so ...\nSkipping the iterations\nPlease come back after populating the same\n")
+    ti.sleep(1)
+    o.system('cls' if pt.system() == 'windows' else 'clear')
+    s.exit()
 
-if len(fext[1 :]) >= 6:
-    print("I will not allow you to have an extension of such large characters")
-    exit()
+for l in LIS:
+    print(f"\n{SYM}\nITERATION -> {itn}\nThe File's Name is ->  {l}\n")
+    try:
+        var = input("\nDo You wanna Rename this (Y/N) : ").strip().lower()
+        if var == 'y':
+            fname = input("Enter the new name of the file here  : ").strip()
+            fext = input("Enter the new extension of the file here : ").strip().lower()
 
-elif " " in fname:
-    print("Enter a valid filename without spaces please")
-    exit()
+            if not fext.startswith('.'):
+                print(f"The Extension {fext} Doesnt Start with a Dot Separator\nAuto-Adding one")
+                fext = '.' + fext
+                print(f"The Extension now becomes {fext}")                
 
-count = 1
+            if len(fext[1 :]) > 6:
+                print(f"Such Long Extensions are not Allowed here\nSkipped the Iteration {itn}\n")
+                continue
 
-for file in list(o.listdir()):
-    if o.path.isdir(file):
-        print("Skipping the Directory Part")
-        continue
-    elif file.endswith('.py'):
-        continue
-    else:
-        final_name = f'{fname}_{count}{fext}'
-        try:
-            base, _ = o.path.splitext(file)
+            if " " in fname:
+                print(f"See my friend while you could make the name with whitespaces but programming languages discourage these practices\nWhy ?\nYou ask you cant run python3 _ _.py or gcc _ _.c you can use '' the behaviour of which is not guaranteed\nSkipping Iteration {itn}")
+                continue
 
-            print(f"Renaming the file :-> {base} to {final_name}\n")
-            o.rename(file , final_name)
+            final_name = f'{fname}{fext}'
+
+            if final_name in list(o.listdir()):
+                print(f"----Skipping this Iteration (aka File Numbered {itn}) to prevent conflicts---")
+                continue
+
+            try:
+                print(f"\nRenaming the file :-> {l} to {final_name}\n")
+                ti.sleep(1)
+                o.rename(l , final_name)
+                
+                ti.sleep(1)
+                print(f"STATUS -> ITERATION {itn}\n{SYM}\nFile name {l} and successfully renamed to {fname}\n{SYM}")
+
+            except Exception as e:
+                print(f"Error Occurred\nIt is this ->{e}")
+                continue
             
-            ti.sleep(1)
-            print(f"STATUS -> ITERATION {count}\n'-'*50\nFile name {base} and successfully renamed to {fname}\n'-'*50")
-            count += 1
-        except Exception as e:
-            print(f"Error Occurred\nIt is this -> {e}")
+        elif var == 'n' :
+            print("\nFile Rename Failed\nOperation aborted by user\n")
             continue
+        else:
+            print("\nFile Rename Failed\nOperation aborted by user\n")
+            continue                
+
+    except (KeyboardInterrupt , EOFError) as kb:
+        print(f"\nSkipping This Iteration\nCurrently on the File Number {itn}")
+        continue
+    
+    finally:
+        itn += 1
+
+print("Made with 🩶 In Python\nCreated by 🄯 RSNPIIT (Ramrup Satpati) from IIT Madras\nReleased under the GNU GPLv3 License")
