2ecc13f0241b1057b2ba11ad49ce73579213d977
RSNPIIT <ramrupsatpati@gmail.com>
2026-04-16 02:06:49 +0530

    Added the Decryption code here

--- sample_encrypt_proj/decrypt.py
+++ sample_encrypt_proj/decrypt.py
@@ -1,33 +1,72 @@
+import time as ti
+import platform as pt
+import sys as s
 import os as o
+import subprocess as sb
 from cryptography.fernet import Fernet as frn
 
-files = []
+# Static and Secret Variables
 FILE = 'thekey.key'
-SECRET_P = 'o.getenv("SECRET_P")
+TARG = 'file1.txt'
+MODE = 'rb'
+AMODE = 'wb'
+SECRET_P = o.getenv("SECRET_P")
+THI = ('.py' ,'.ipynb')
+LIS = [f for f in o.listdir() if o.path.isfile(f) and not f.endswith(THI)]
 
 if not SECRET_P:
-    print("You Dont have the necessary permission here")
-    exit()
+    print("You Dont have the necessary permission here\nPlease add one Secret Phrase ")
+    ti.sleep(1)
+    o.system('cls' if  pt.system() == 'windows' else 'clear')
+    s.exit()
 
-for file in o.listdir():
-    if file == 'malware_create.py' or file == 'thekey.key' or file == 'decrypt.py':
+if not LIS:
+    print("Python Found out that there is no target sample file in the Directory\nPlease make some using touch command\n")
+    ti.sleep(1)
+    o.system('cls' if  pt.system() == 'windows' else 'clear')
+    s.exit()
+
+elif FILE not in LIS:
+    print(f"The File {FILE} not found\nPlease Run and generate one using Encrption Encrypt.py\n")
+    ti.sleep(1)
+    o.system('cls' if  pt.system() == 'windows' else 'clear')
+    s.exit()
+
+with open(TARG , MODE) as f:
+    samp = f.read(1)
+    if samp != b'g':
+        print(f"Wait The File is in Plain Site View\nThat means Encrypt.Py has not run yet\n")
+        print("Running Encrypt.py\n")
+        ti.sleep(1)
+        try:
+            jkl = sb.run(
+                ['python3' , 'encrypt.py'],
+                capture_output = True,
+                text = True,
+                check = True
+            )
+        except Exception as e:
+            ti.sleep(1)
+            o.system('cls' if  pt.system() == 'windows' else 'clear')
+            s.exit()
+        else:
+            print(jkl.stdout)
+
+with open(FILE , MODE) as k:
+    key = k.read()
+
+for file in LIS:
+    if file == FILE:
         continue
-    if o.path.isfile(file):
-        files.append(file)
+    
+    with open(file , MODE) as fl:
+        rfl = fl.read()
+    try:
+        dec_con = frn(key).decrypt(rfl)
 
-print(files)
+        with open(file , AMODE) as z:
+            z.write(dec_con)
 
-with open(FILE , 'rb') as k:
-    secr = k.read()
-
-ph = input("Enter your Secret Phase : ").strip().lower()
-
-if ph == SECRET_P:
-    for file in files:
-        with open(file , 'rb') as fl:
-            con = fl.read()
-        dec_con = frn(secr).decrypt(con)
-        with open(file , 'wb') as wl:
-            wl.write(dec_con)
-else:
-    print("Not the Intended person\nOr Maybe Try again and nope I'm not gonna help you")
+        print(f"Status\nDecrypted the file {file} successsfully\n")
+    except Exception as e:
+        print(f"Skipping File -> {file}\nIt is Due to error {e}")
