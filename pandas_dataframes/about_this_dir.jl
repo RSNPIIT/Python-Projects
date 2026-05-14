3c6ecf3d5b6ee4827bb7fca011a00551c3b69349
RSNPIIT <ramrupsatpati@gmail.com>
2026-05-14 18:10:21 +0530

    Added the Code for Pandas DataFrames Here

---
+++ pandas_dataframes/about_this_dir.jl
@@ -0,0 +1,12 @@
+# Triple quotes allow you to format text across multiple lines exactly as written
+info_message = """
+============================================================
+DIRECTORY PURPOSE
+============================================================
+This Directory is based upon DataFrames and their use cases 
+in Pandas Python.
+============================================================
+"""
+
+printstyled(info_message, bold=true, color=:green)
+
