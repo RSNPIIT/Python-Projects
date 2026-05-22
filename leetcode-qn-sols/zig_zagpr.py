f4a9971316f9b7186887d18b6046cce3a587c762
RSNPIIT <ramrupsatpati@gmail.com>
2026-05-23 03:06:04 +0530

    Added the ZigZag Leetcode sollution Day6 here

---
+++ leetcode-qn-sols/zig_zagpr.py
@@ -0,0 +1,16 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or len(s) <= numRows:
+            return s
+        res = [''] * numRows
+        index = 0
+        step = 1
+        for ch in s:
+            res[index] += ch
+
+            if index == 0:
+                step = 1
+            elif index == numRows - 1:
+                step = -1
+            index += step
+        return ''.join(res)
