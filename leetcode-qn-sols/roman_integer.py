1fccb977d86f023fdf8c779108fb4feadc6f8448
RSNPIIT <ramrupsatpati@gmail.com>
2026-05-23 18:35:02 +0530

    Leetcode Qn 13 -> Roman to Integer solved (Note - I just made the Dictionary Global to make this commit thats it so the previous commit on this file can be taken instead)

--- leetcode-qn-sols/roman_integer.py
+++ leetcode-qn-sols/roman_integer.py
@@ -1,17 +1,18 @@
+roman_map = {
+    'I' : 1,
+    'V' : 5,
+    'X' : 10,
+    'L' : 50,
+    'C' : 100,
+    'D' : 500,
+    'M' : 1000
+} 
+
 class Solution:
-    def roman_to_integer(self, num: int) -> str:
-        roman_mapp = {
-            'I' :  1,
-            'V' :  5,
-            'X' :  10,
-            'L' :  50,
-            'C' :  100,
-            'D' :  500,
-            'M' :  1000 
-        }
+    def romanToInt(self, s: str) -> int:
         res = 0
-        for i in range(len(s)):
-            if i < len(s) - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
+        for i in range(len((s))):
+            if i < len(s) - 1 and roman_map[s[i]] < roman_map[s[i+1]]:
                 res -= roman_map[s[i]]
             else:
                 res += roman_map[s[i]]
