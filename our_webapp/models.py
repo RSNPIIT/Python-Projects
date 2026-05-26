4ca87c31dadd6da7bcebc7df457541760d91cf0f
RSNPIIT <ramrupsatpati@gmail.com>
2026-05-26 12:57:51 +0530

    Added the Models and DB here

--- our_webapp/models.py
+++ our_webapp/models.py
@@ -1,4 +1,5 @@
 from extensions import db
+from flask_login import UserMixin
 
 class Person(db.Model):
     __tablename__ = 'people'
@@ -9,3 +10,23 @@ class Person(db.Model):
 
     def __repr__(self):
         return f'Person with name {self.name} and age {self.age}'
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    uid = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    username = db.Column(
+        db.Text,
+        unique=True,
+        nullable=False
+    )
+    password = db.Column(
+        db.Text,
+        nullable=False
+    )
+    is_admin = db.Column(
+        db.Boolean,
+        default=False
+    )
