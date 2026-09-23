01001afcae300c59efb5e9dfca83415ad2b86963
RSNPIIT <ramrupsatpati@gmail.com>
2026-09-23 18:15:24 +0530

    Made the code more error proof via try/except case handling

--- word_pair.py
+++ word_pair.py
@@ -1,16 +1,33 @@
-n = abs(int(input("Enter the number of sentences you wanna get a pair of :-> ")))
-SLIST = []
-WLIST = set()
+import sys as s
+import time as ti
+import os as o
 
-for i in range(n):
-    sent = input(f"Enter the sentence number {i+1} here :-> ").strip().lower()
-    SLIST.append(sent)
+try:
+    n = abs(int(input("Enter the number of sentences you wanna get a pair of :-> ")))
 
-for sente in SLIST:
-    words = sente.split()
+except ValueError as v:
+    print("Error Happened here\nPlease retry")
+    o.system("cls" if o.name == 'nt' else "clear")
+    ti.sleep(3)
+    s.exit()
 
-    for i in range(len(words) - 1):
-        pair = (words[i], words[i+1])
-        WLIST.add(pair)
+else:
+    SLIST = []
+    WLIST = set()
 
-print(f"The Word Pairs formed here are -> {len(WLIST)}")
+    for i in range(n):
+        sent = input(f"Enter the sentence number {i+1} here :-> ").strip().lower()
+        SLIST.append(sent)
+
+    for sente in SLIST:
+        words = sente.split()
+
+        for i in range(len(words) - 1):
+            pair = (words[i], words[i+1])
+            WLIST.add(pair)
+
+    ti.sleep(2)
+    print(f"The Word Pairs formed here are -> {len(WLIST)}")
+
+finally:
+    print("Copyleft | Ramrup Satpati | 2026 | IIT Madras | Released under the GNU GPLv3 license")
